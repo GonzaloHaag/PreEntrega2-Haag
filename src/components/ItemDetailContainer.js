1bec03cfe8384a75a12c52524cb542a41cbc2ce0
GonzaloHaag <111447167+GonzaloHaag@users.noreply.github.com>
2023-01-23 18:53:20 -0300

    Logre el filtrado de productos por category

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
-const ItemDetailContainer = () => {
+const ItemDetailContainer = (producto) => {
 
   return(
-      <div>ItemDetailsContainer</div>
+      <div>
+        <h1>ItemDetailContainer</h1>
+      </div>
   )
 }
 
